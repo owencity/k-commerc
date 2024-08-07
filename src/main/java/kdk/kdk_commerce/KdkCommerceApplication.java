c58020adcf985804a6856bfa4183295098706464
owencity <skyko6530@gmail.com>
2024-08-07 22:55:40 +0900

    기본 트리 완성

---
+++ src/main/java/kdk/kdk_commerce/KdkCommerceApplication.java
@@ -0,0 +1,13 @@
+package kdk.kdk_commerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KdkCommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KdkCommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kdk/kdk_commerce/KdkCommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package kdk.kdk_commerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KdkCommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
